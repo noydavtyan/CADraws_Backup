66563df0026b3852b4940736e9785dfb176299c6
Noy <noydavtyan@mail.ru>
2021-07-14 18:53:52 +0200

    Ready

---
+++ create_excel.js
@@ -0,0 +1,54 @@
+const axios = require('axios');
+const excel = require('excel4node');
+
+var workbook = new excel.Workbook();
+let worksheet = workbook.addWorksheet('Models');
+let i = 1;
+let date_ob = new Date();
+
+
+const date = ("0" + date_ob.getDate()).slice(-2);
+const month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+const year = date_ob.getFullYear();
+const hours = date_ob.getHours();
+const minutes = date_ob.getMinutes();
+const seconds = date_ob.getSeconds();
+const fileName = year + "-" + month + "-" + date + "_" + hours + "-" + minutes + "-" + seconds;
+
+const fetchRows = () => {
+
+    axios.request({
+        url: "http://localhost:8080/models",
+        method: "GET",
+    })
+    .then((response) => {
+        worksheet.cell(i,1).string("ID");
+        worksheet.cell(i,1).string("Company");
+        worksheet.cell(i,2).string("Description");
+        worksheet.cell(i,3).string("Order Date");
+        worksheet.cell(i,4).string("Delivery Date");
+        worksheet.cell(i,5).string("Worker Price");
+        worksheet.cell(i,6).string("Total Price");
+        worksheet.cell(i,7).string("Payed");
+        worksheet.cell(i,8).string("Received");
+        response.data.forEach(model => {
+            i++;
+            worksheet.cell(i,1).string(model.id);
+            worksheet.cell(i,2).string(model.company.name);
+            worksheet.cell(i,3).string(model.description);
+            worksheet.cell(i,4).string(model.worker_price);
+            worksheet.cell(i,5).string(model.total_price);
+            worksheet.cell(i,6).string(model.orderDate);
+            worksheet.cell(i,7).string(model.delivery_date);
+            worksheet.cell(i,8).string(model.payed);
+            worksheet.cell(i,9).string(model.received);
+        });
+        workbook.write(fileName + '.xlsx');
+        })
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
+
+fetchRows();
